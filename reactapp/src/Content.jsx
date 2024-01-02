5ade64c5db35ab84b4bb2af885791d5cb308edb8
Ashlee Geter <64159001+prettygenius@users.noreply.github.com>
2024-01-02 00:32:39 -0600

    Update Content.jsx

--- reactapp/src/Content.jsx
+++ reactapp/src/Content.jsx
@@ -16,9 +16,3 @@ function Content({items,handleCheck,handleDelete}) {
         
 export default Content
 
-//  true if does not match keep false does match delete out of arr
-//function handleDelete(id) {
-//store the new filtered arr delete id if match
-//   const newBlogs = blogs.filter((blog) => blog.id !== id);
-//   setBlogs(newBlogs);
-// }
